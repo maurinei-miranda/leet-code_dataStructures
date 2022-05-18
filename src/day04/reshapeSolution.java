1051355e764c0b18c5b8545345de8e0040aef4ea
maurinei-miranda <maurinei.develop@gmail.com>
2022-05-18 18:26:12 -0300

    initial reshape
    Signed-off-by: maurinei-miranda <maurinei.develop@gmail.com>

---
+++ src/day04/reshapeSolution.java
@@ -0,0 +1,12 @@
+package day04;
+
+public class reshapeSolution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] matrixReshape(int[][] mat, int r, int c) {
+
+
+    }
+}
